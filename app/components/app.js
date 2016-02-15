ddbf802f2dae14cf017fc8d67fbe11b674105a31
Jordan Papaleo <papaleowebdev@gmail.com>
2016-02-14 16:55:38 -0800

    Pushing complete example

--- app/components/app.js
+++ app/components/app.js
@@ -8,18 +8,30 @@ export default class App extends Component {
   }
 
   render () {
+    const footerIcon = require('images/tahoe-js-logo.gif')
+
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-12'>
-            <SearchBar />
+      <div className='container-fluid app-wrapper'>
+        <header className='app-header'>
+          <h1><img className='header-logo' src={footerIcon} style={{width: 50}} /> Redux Weather Example App</h1>
+        </header>
+
+        <section className='app-content'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <SearchBar />
+            </div>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col-md-12'>
-            <WeatherList />
+          <div className='row'>
+            <div className='col-md-12'>
+              <WeatherList />
+            </div>
           </div>
-        </div>
+        </section>
+
+        <footer className='app-footer'>
+          BeerJS:Tahoe - Jordan Papaleo
+        </footer>
       </div>
     )
   }
